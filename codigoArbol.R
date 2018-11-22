a0173883185eafb7e53aa8b544ab6bc315915bed
cesarhormazabal <hormazabal.cesar@gmail.com>
2018-11-21 23:59:59 -0300

    Codigo Comentado

--- codigoArbol.R
+++ codigoArbol.R
@@ -2,24 +2,31 @@ data_banco <- read.csv(
   "https://raw.githubusercontent.com/cesarhormazabal/predictive-_analytics/master/Marketing%20Laboral/bank-full.csv", 
   sep=";")
 
-head(data_banco)
 
+#Cargar liberías
 library(caret) #Métodos de ML
 library(tidyverse) #Librería hecha, facilitadores de la vida
 library(rpart) # Árboles de decisión
 library(e1071) # Modelos de ML
 library(ranger) # Bosques de decisión 
 
+#Revisar si está todo bien cargado
 str(data_banco)
+head(data_banco)
+summary(data_banco)
 
+#¿Qué data varía poc?
 data_banco%>%
   nearZeroVar()->c.casi.cero.var
 
 names(data_banco)[c.casi.cero.var]
 
+#Me quedo con la data que no varía poco, y le quito las columnas
+#que no me sirven
 data_banco%>%
   select(-default,-pdays,-day,-month,-contact)->data_banco
 
+#Genero mi partición en data de entrenamiento y validación
 train_index <- createDataPartition(data_banco$y,
                                    p = 0.8,
                                    list = FALSE,
@@ -28,45 +35,53 @@ train_index <- createDataPartition(data_banco$y,
 banco_entrenamiento<-data_banco[train_index,]
 banco_validacion<-data_banco[-train_index,]
 
+#Reviso si funcionó bien
 str(banco_entrenamiento)
+
 
 #Entrenamiento de árbol
 rf_fit <- train(y ~ ., 
                 data = banco_entrenamiento, 
                 method = "rpart")
-
+#Predicción de árbol
 prediccion.arbol1<-predict(rf_fit,banco_validacion,type="prob")
 
-
+#Generar la data necesaria para la curva ROC
 rocdata.arbol <- data.frame(
   D = banco_validacion$y
   ,M = prediccion.arbol1$yes
   , Z = rep("Arbol",dim(banco_validacion)[1])
 )
-roc<-ggplot(rocdata.arbol, aes(m = M, d = D,color=Z)) + geom_roc()
 
-x11()
+#Dibujar la curva roc
+roc<-ggplot(rocdata.arbol, aes(m = M, d = D,color=Z)) + geom_roc()
 roc
 
 
 #Entrenamiento de Bosque
+#Importance determina que guardaré la importancia de las variables
+#probability=TRUE significa que guardaré la probabilidad de pertenecer a alguna clase
 rf_fit.ranger <- ranger(y ~ ., 
                 data = banco_entrenamiento,
                 importance="impurity",
                 probability = TRUE
                 )
 
+#Importancia de las variables según el nivel de impureza
 ranger::importance(rf_fit.ranger)
 
+#Guardar Modelo
 saveRDS(rf_fit.ranger,"modelo_bosque.rds")
 
+#predecir el modelo
 prediccion.bosque<-predict(rf_fit.ranger,banco_validacion)
 
-prediccion.bosque%>%
-  head
 
+#Librería
+install.packages("plotROC")
 library(plotROC)
 
+#Data necesaria para generar la curva ROC
 rocdata.bosque <- data.frame(
   D = banco_validacion$y
   ,M = prediccion.bosque$predictions[,"yes"]
@@ -74,22 +89,25 @@ rocdata.bosque <- data.frame(
 )
 
 
-
+#Entrenamiento de data (árbol con riesgo de sobreajuste)
 fit3 <- rpart(y ~ .,
               data = banco_entrenamiento, 
               control = rpart.control(cp = 0.00000000001))
 
+#Generar predicción
 prediccion.arbol.2<-predict(fit3,banco_validacion,type="prob")
 
-
+#Data necesaria para generar la curva ROC
 rocdata.arbol.2 <- data.frame(
   D = banco_validacion$y
   ,M = prediccion.arbol.2[,"yes"]
   , Z = rep("Arbol Sobre Ajustado",dim(banco_validacion)[1])
 )
 
+#Unir con los otros modelos para comparar
 rocdata<-bind_rows(rocdata.arbol,rocdata.bosque,rocdata.arbol.2)
 
+#Gráfico
 roc<-ggplot(rocdata, aes(m = M, d = D,color=Z)) + geom_roc()
 
 x11()
